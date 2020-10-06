0da5b02bed1c9788732d02ce339f44a3d547c1a3
khalil saidane <khalilsaidane@khalils-MacBook-Pro.local>
2020-10-06 15:52:34 +0200

    Documenting get_service and get_repository

--- api/dependencies/database.py
+++ api/dependencies/database.py
@@ -16,13 +16,15 @@ def get_db():
         db.close()
 
 
+# Every repository depends on a session provided by get_db
 def get_repositories(*repositories):
-    def _get_repositories(session: Session = Depends(get_db)):
-        """
+    """
 
-        :param session:
-        :return:  List of instantiated repositories that will be injected in the service
-        """
+    :param repositories: Non-Keyword Arguments of Repository classes that inherit from BaseRepository.
+    :return: A list of instantiated repositories, and pass the session to the constructor.
+    """
+
+    def _get_repositories(session: Session = Depends(get_db)):
         instantiated_repositories = []
         for repo in repositories:
             instantiated_repositories.append(repo(session))
@@ -31,7 +33,14 @@ def get_repositories(*repositories):
     return _get_repositories
 
 
+# Every service depends on a list of repositories provided by get_repositories
 def get_service(service_type: Type[BaseService]):
+    """
+
+    :param service_type: Service Class that inherits from BaseService.
+    :return: An instance of the service, and pass the repositories to the constructor.
+    """
+
     def _get_service(repositories: Type[List[BaseRepository]] = Depends(get_repositories(*service_type.repositories))):
         return service_type(*repositories)
 
--- db/database.py
+++ db/database.py
@@ -7,6 +7,6 @@ from core.config import get_settings
 Base = declarative_base()
 settings = get_settings()
 engine = create_engine(
-        settings.database_url, connect_args={"check_same_thread": False}
-    )
+    settings.database_url, connect_args={"check_same_thread": False}
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
