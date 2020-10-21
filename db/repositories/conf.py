4b7bd85f17383a15e2080543166622eaacf745e3
khalil saidane <khalilsaidane@khalils-MacBook-Pro.local>
2020-10-21 15:57:28 +0200

    adding new implementation

--- db/repositories/conf.py
+++ db/repositories/conf.py
@@ -1,6 +1,8 @@
-from db.repositories.implementations.item import ItemRepositoryImpl
+from db.repositories.implementations.item import ItemRepositoryImpl, ItemRepositoryNewImpl
 from db.repositories.interfaces.item import IItemRepository
-
+"""
+    In this dic we assign the implementation to the interface
+"""
 repository_dict = {
-    IItemRepository: ItemRepositoryImpl
+    IItemRepository: ItemRepositoryNewImpl
 }
--- db/repositories/implementations/item.py
+++ db/repositories/implementations/item.py
@@ -6,3 +6,14 @@ from ..interfaces.item import IItemRepository
 
 class ItemRepositoryImpl(CRUDBase[Item, ItemCreateSchema, ItemCreateSchema], IItemRepository):
     model: Item = Item
+
+
+class ItemRepositoryNewImpl(CRUDBase[Item, ItemCreateSchema, ItemCreateSchema], IItemRepository):
+    model: Item = Item
+
+    def get(self, id: int):
+        return {
+            "title": "item",
+            "description": "New Item..",
+            "id": 2,
+        }
